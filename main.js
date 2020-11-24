8d6acc5731df0e834650b1ab58480b2271dfbb1d
ObikaKelvin <obikakelvin.2255@gmail.com>
2020-11-24 18:53:27 +0100

    made hamburger responsive menu

--- main.js
+++ main.js
@@ -11,3 +11,20 @@
 // const dotItems = document.querySelectorAll('.dots__items');
 
 // console.log(dotItems);
+
+document.querySelector('.menu__image--menu').addEventListener("click", function() {
+    console.log(this);
+    document.querySelector('.collapse').style.display = 'block';
+    document.querySelector('.menu__toggle').style.backgroundColor = '#f1f1f1';
+    document.querySelector('.menu__image--close').style.display = 'block';
+    this.style.display = 'none';
+});
+
+document.querySelector('.menu__image--close').addEventListener("click", function() {
+    console.log(this);
+    document.querySelector('.collapse').style.display = 'none';
+    document.querySelector('.menu__toggle').style.backgroundColor = '#fff';
+    document.querySelector('.menu__image--menu').style.display = 'block';
+    this.style.display = 'none';
+});
+
