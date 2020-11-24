187bc90121d5a60b6cafc6d700875aab4b3f619f
ObikaKelvin <obikakelvin.2255@gmail.com>
2020-11-24 22:00:16 +0100

    removed comments

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-// const dots = document.querySelectorAll('.dots');
-// const state = 50;
-// dots.forEach(cur => {
-//     document.addEventListener("click", (el, index) => {
-//         let translate = index;
-//         const dotItems = document.querySelectorAll('.dots__items');
-//         dotItems.style.transform = `translateX(200)`;
-//     });
-// });
 
-// const dotItems = document.querySelectorAll('.dots__items');
-
-// console.log(dotItems);
 
 document.querySelector('.menu__image--menu').addEventListener("click", function() {
     console.log(this);
