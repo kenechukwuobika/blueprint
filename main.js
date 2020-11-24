b0623c7ed8803d96124fad6aec0616a1b1c1c3ae
ObikaKelvin <obikakelvin.2255@gmail.com>
2020-11-24 06:13:49 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// const dots = document.querySelectorAll('.dots');
+// const state = 50;
+// dots.forEach(cur => {
+//     document.addEventListener("click", (el, index) => {
+//         let translate = index;
+//         const dotItems = document.querySelectorAll('.dots__items');
+//         dotItems.style.transform = `translateX(200)`;
+//     });
+// });
+
+// const dotItems = document.querySelectorAll('.dots__items');
+
+// console.log(dotItems);
