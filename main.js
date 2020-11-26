7218f390781a8b42a92d776acea048062434b1e1
ObikaKelvin <obikakelvin.2255@gmail.com>
2020-11-26 14:24:52 +0100

    fixed image slider

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-
-
 document.querySelector('.menu__image--menu').addEventListener("click", function() {
-    console.log(this);
     document.querySelector('.collapse').style.display = 'block';
     document.querySelector('.menu__toggle').style.backgroundColor = '#f1f1f1';
     document.querySelector('.menu__image--close').style.display = 'block';
@@ -9,7 +6,6 @@ document.querySelector('.menu__image--menu').addEventListener("click", function(
 });
 
 document.querySelector('.menu__image--close').addEventListener("click", function() {
-    console.log(this);
     document.querySelector('.collapse').style.display = 'none';
     document.querySelector('.menu__toggle').style.backgroundColor = '#fff';
     document.querySelector('.menu__image--menu').style.display = 'block';
@@ -57,3 +53,61 @@ window.addEventListener("scroll", function() {
     
      lastScrollTop = st <= 0 ? 0 : st;
 });
+
+window.addEventListener("touchmove", function() {
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+    // let offsetBottom = document.querySelector('.info__bar').offsetTop + document.querySelector('.info__bar').offsetHeight
+    if (window.pageYOffset > document.querySelector('.info__bar').offsetTop) {
+
+        document.querySelector('.info__bar').classList.add( 'sticky');
+        header.style.transform = "translateY(-10rem)";
+    } 
+    else {
+        document.querySelector('.info__bar').classList.remove('sticky');
+    }
+
+    if (st < lastScrollTop){
+        header.style.transform = "translateY(0)";
+    }
+    
+     lastScrollTop = st <= 0 ? 0 : st;
+});
+
+
+function showSlide(slide) {
+    let margin = 0;
+    const transformConstant = (slide ) * 100;
+    margin += transformConstant;
+    if(margin > 300){
+        margin = 300;
+    }
+    else if(margin < 0){
+        margin = 0
+    }
+
+    document.querySelectorAll('.img__slider--item').forEach(el => {
+        el.style.transform = `translateX(-${margin}%)`;
+    });
+
+    showCurrentDot(slide);
+}
+
+function showCurrentDot(slide) {
+    document.querySelectorAll('.dots').forEach((element, index) => {
+        if(index === slide){
+            element.classList.add('dots__active');
+        }else{
+            element.classList.remove('dots__active');
+        }
+
+    });
+}
+
+document.querySelectorAll('.dots').forEach((el, index) => {
+    el.addEventListener('click', e => {
+        e.preventDefault();
+        showSlide(index);
+    });
+});
+
+
